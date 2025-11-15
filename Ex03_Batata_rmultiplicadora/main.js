7871f8b06b667f9efe86083f7b7448b27999c304
m1st1nh0 <lukaschamposki@gmail.com>
2025-11-14 21:51:27 -0300

    Salvando alterações antes de atualizar

--- Ex03_Batata_rmultiplicadora/main.js
+++ Ex03_Batata_rmultiplicadora/main.js
@@ -1,8 +1,15 @@
-function batataMultiplicadora(batata){
-  for (let i = 1; i < 11; i++) {
-    console.log(`${batata} X ${i} = ${batata*i}`)
-    
-  }
-}
+const areaDisplay = document.getElementById("display");
 
-batataMultiplicadora(3)
+document.getElementById("enviar").onclick = function batataMultiplicadora() {
+  let menssagem = [];
+  const batata = document.getElementById("input-string").value;
+  if (Number(batata != isNaN && batata > 0 && batata < 10)) {
+    document.getElementById("input-string").value = "";
+    for (let i = 1; i < 11; i++) {
+      menssagem.push(`${batata} X ${i} = ${batata * i}`);
+    }
+    areaDisplay.innerHTML = menssagem.join("<br>");
+    return;
+  }
+  areaDisplay.innerHTML = "digite um número<br>até 10";
+};
